d7750f95d24f36e9bd569d1894046878e9daa42a
0xmani <35402359+0xmani@users.noreply.github.com>
2024-05-19 23:08:07 +0530

    Update noursubnet.sh

--- noursubnet.sh
+++ noursubnet.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-for i in {1..254} ; 
-        for r in {1..254} ;
-                for n in {1..254};
-                        for m in {1.254};
-                            do (proxychains4 ping -c 1 $i.$r.$n.$m > /dev/null 2>&1 | grep "bytes from" &) ;done
-
+for i in {1..254}; do
+        for r in {1..254}; do
+                for n in {1..254}; do
+                        for m in {1.254};do
+                                (proxychains4 ping -c 1 $i.$r.$n.$m > /dev/null 2>&1 | grep "bytes from" &) ;
+done
+done
+done
+done
